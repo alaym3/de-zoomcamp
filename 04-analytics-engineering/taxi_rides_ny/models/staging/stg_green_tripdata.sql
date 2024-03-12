b8c485d884647511389bb2250adf97cd27f36364
Alaina Martinez <alaina.ann.martinez@gmail.com>
2024-03-12 14:52:12 +0000

    add model

---
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -0,0 +1,39 @@
+with 
+
+source as (
+
+    select * from {{ source('staging', 'green_tripdata') }}
+
+),
+
+renamed as (
+
+    select
+        vendor_id,
+        lpep_pickup_datetime,
+        lpep_dropoff_datetime,
+        store_and_fwd_flag,
+        ratecode_id,
+        pu_location_id,
+        do_location_id,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        extra,
+        mta_tax,
+        tip_amount,
+        tolls_amount,
+        ehail_fee,
+        improvement_surcharge,
+        total_amount,
+        payment_type,
+        {{ get_payment_type_description('payment_type')}} as payment_type_description,
+        trip_type,
+        congestion_surcharge,
+        lpep_pickup_date
+
+    from source
+
+)
+
+select * from renamed
